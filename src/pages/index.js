54adca1edeb39ddbbbba87c94a548f9aae892d07
nicoGelato <86999857+NicoGelato@users.noreply.github.com>
2022-07-02 02:34:53 -0300

    Hardcodee el Helmet, hay que pasar a gatsby-confing

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import {
   StyledEngineProvider,
 } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import Helmet from "../components/Helmet";
+import  Helmet  from "react-helmet";
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 // import News from "../components/News";
@@ -41,6 +41,32 @@ const IndexPage = () => {
 
   return (
     <StyledEngineProvider injectFirst>
+      <Helmet>
+        <title>Otra Cosa</title>
+        <meta property="og:title" content="Otra Cosa" />
+        <meta property="og:site_name" content="Otra Cosa" />
+        <meta
+          name="description"
+          content="Pagina oficial de la banda Otra Cosa"
+        />
+        <meta
+          property="og:description"
+          content="Pagina oficial de la banda Otra Cosa"
+        />
+        <meta
+          name="keywords"
+          content="otra cosa, banda, rock, musica, canciones, recitales, presentaciones, pagina, oficial"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://otracosa.gatsbyjs.io/static/icon-a3ca93a2d3bece678d680e1219343071.png"
+        />
+        <meta property="og:locale" content="es_AR" />
+        <meta property="og:locale:alternate" content="es_LA" />
+        <meta property="og:url" content="http://otracosa.gatsbyjs.io" />
+        <link rel="canonical" href="http://otracosa.gatsbyjs.io" />
+      </Helmet>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Helmet />
